1e5c62338c8e2f9189af9716868f60de6bcb041f
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-01-14 14:29:51 +1100

    Added API query function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,21 @@
 // openweathermap.org for generating api keys to generate info.
 // Tailwind for CSS
 // Jquery
+var apiKey = '618119deb4a6658d9ee905a42da62d94';
+var searchButton = $('input[name="search-btn"');
+
+function handleAPIQuery() {
+    var city = $('input[name="city-search"').val();
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + ',au&appid=' + apiKey;
+
+    fetch(queryURL, {
+        method: 'GET'
+    })
+    .then(function (response) {
+        console.log(response);
+        return response;
+    })
+    .then(function (data) {
+        console.log(data.main);
+    });
+}
