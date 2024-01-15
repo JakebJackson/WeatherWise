ea11bcdba2d1bbb38e9756922def4d0d9ee60b8e
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-01-16 10:41:04 +1100

    Finished and finalised HTML

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,15 @@
 // openweathermap.org for generating api keys to generate info.
 // Tailwind for CSS
 // Jquery
-var apiKey = '618119deb4a6658d9ee905a42da62d94';
-var searchButton = $('input[name="search-btn"');
+var apiKey = '73f62982a1a8b5e56dcd92695b5033a1';
 
-function handleAPIQuery() {
+function handleAPIQuery(event) {
+    event.preventdefault();
+    
+    console.log("button clicked")
     var city = $('input[name="city-search"').val();
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + ',au&appid=' + apiKey;
+    console.log("button clicked, search value:" + city)
 
     fetch(queryURL, {
         method: 'GET'
@@ -16,6 +19,11 @@ function handleAPIQuery() {
         return response;
     })
     .then(function (data) {
-        console.log(data.main);
+        console.log(data);
+        var forecastDays = [];
+        var fiveDayFilter = new Date(forecast.dt_txt).getDate();
+        if (!forecastDays.includes(forecastTime)) {
+            return forecastDays.push(forecastTime)
+        }
     });
 }
