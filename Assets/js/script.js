2dc8579a304f7d9ca0eb98e3c9984e412325a223
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-01-16 23:10:59 +1100

    Finished application.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,12 +7,33 @@ var forecastDiv = $('#forecast-div');
 // Global variables/arrays/objects.
 var daySelect = 0;
 var forecastData = [];
-var currentDayData= {};
+var currentDayData = {};
+
+// Checks to see if the user has any previous searches on page load
+if (localStorage.getItem('searchList') == null) {
+    // If not then creates the searchList object
+    var searchList = {};
+} else {
+    // if so then retrieves the prior searches
+    var searchList = JSON.parse(localStorage.getItem('searchList'));
+
+    // Generates buttons based on all of the prior searches until the object runs out of prior searches.
+    for (i = 0; i < Object.keys(searchList).length; i++) {
+        var oldSearchButton = document.createElement('button');
+        oldSearchButton.textContent = searchList[Object.keys(searchList)[i]];
+        oldSearchButton.setAttribute('id', searchList[Object.keys(searchList)[i]]);
+        oldSearchButton.className = 'btn btn-secondary btn-large p-1 m-1 w-100';
+        oldSearchButton.setAttribute('onclick', 'handleGetFromLocalStorage(event)');
+        oldSearchButton.setAttribute('value', searchList[Object.keys(searchList)[i]]);
+        $('#search-history').prepend(oldSearchButton);
+    }
+}
 
 
 function handleAPIQuery() {
     // Prevents refresh on button click.
     event.preventDefault();
+    daySelect = 0;
 
     // Gets the value that will be used in the fetch url
     var city = searchBtn.val();
@@ -41,8 +62,6 @@ function handleAPIQuery() {
             WeatherDesc: data.list[0].weather[0].description,
             conditions: data.list[0].weather[0].main,
         };
-
-        // Creating an array for the forecast data.
         
         // This variable will be used in the for loop to select the next day.
         console.log(data);
@@ -65,9 +84,12 @@ function handleAPIQuery() {
             }
         }
 
+        if(searchList[currentDayData.name] == null) {
+            searchList[currentDayData.name] = currentDayData.name;
+        }
+
         // Logs forecastData
         console.log(forecastData);
-        
 
         // Logs currentDayData object
         console.log(currentDayData);
@@ -76,16 +98,18 @@ function handleAPIQuery() {
         handleDisplayResults();
 
         // Returns all used arrays.
-        return currentDayData, forecastData, data;
+        return currentDayData, forecastData, data, searchList;
     });
 }
 
 function handleDisplayResults() {
+    // Sets text on the page to the relevant information.
     cityName.text(currentDayData.name + ' (' + currentDayData.date + ')');
     $('#cur-temp').text(currentDayData.temp);
     $('#cur-wind').text(currentDayData.wind);
     $('#cur-humidity').text(currentDayData.humidity);
 
+    // Unhides the 5 day forecast div.
     forecastDiv.removeClass('d-none');
 
     // For loop to generate the 5 day forecast cards
@@ -99,17 +123,38 @@ function handleDisplayResults() {
         if (forecastData[i - 1].conditions == 'clear') {
             // currentCard.children('img')
         }
+        // Calls handleSaveToLocalStorage function.
+        handleSaveToLocalStorage();
     };
-    handleSaveToLocalStorage();
+    
 }
 
 function handleSaveToLocalStorage() {
+    // Saves all of the objects to local storage for later use.
     localStorage.setItem('storedData' + currentDayData.name, JSON.stringify(currentDayData));
     localStorage.setItem('storedForecastData' + currentDayData.name, JSON.stringify(forecastData));
-    // Create a button for the new storage, set the value of the button to currentDayData.name
+    localStorage.setItem('searchList', JSON.stringify(searchList));
+    // Variable that checks if this element with this id already exists.
+    var elementExists = document.getElementById(currentDayData.name);
+    // if not then runs the handle save to local storage function.
+    if (elementExists == null) {
+        // Create a button for the prior searches.
+        var newButton = document.createElement('button');
+        newButton.textContent = currentDayData.name;
+        newButton.setAttribute('id', currentDayData.name);
+        newButton.className = 'btn btn-secondary btn-large p-1 m-1 w-100';
+        newButton.setAttribute('onclick', 'handleGetFromLocalStorage(event)');
+        newButton.setAttribute('value', currentDayData.name);
+        $('#search-history').prepend(newButton);
+    }
 }
 
-function handleGetFromLocalStorage() {
-    currentDayData = JSON.parse(localStorage.getItem('storedData' + $(this).value));
-    forecastData = JSON.parse(localStorage.getItem('storedForecastData' + $(this).value));
+// Gets items back from localstorage
+function handleGetFromLocalStorage(event) {
+    var val = event.target.value;
+    currentDayData = JSON.parse(localStorage.getItem('storedData' + val));
+    forecastData = JSON.parse(localStorage.getItem('storedForecastData' + val));
+
+    // Calls handleDisplayResults function to display the items retrieved from local on the webpage
+    handleDisplayResults();
 }
